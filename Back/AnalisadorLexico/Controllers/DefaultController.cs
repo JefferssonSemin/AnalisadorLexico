e75861e5f1f5bb130d0fdd0b39fc79c7d424ca02
Flp <felipeputsch@unifebe.edu.br>
2018-12-11 20:13:29 -0200

    aa

--- Back/AnalisadorLexico/Controllers/DefaultController.cs
+++ Back/AnalisadorLexico/Controllers/DefaultController.cs
@@ -22,57 +22,66 @@ namespace AnalisadorLexico.Controllers
 
         [HttpPost]
         [Route("VerificarTexto")]
-        public bool VerificarTexto([FromBody]Model modelo)
+        public string VerificarTexto([FromBody]Model modelo)
         {
             bool valido = true;
             modelo.Texto = modelo.Texto.ToUpper();
+            if ((modelo.Texto.Split("{").Length - 1) != (modelo.Texto.Split("}").Length - 1))
+            {
+                return "Erro: quantidade de { diferente de }";
+            }
+            modelo.Texto = modelo.Texto.Replace("{", "");
+            modelo.Texto = modelo.Texto.Replace("}", "");
             string[] stringSeparators = new string[] { "\r\n" };
             string[] lines = modelo.Texto.Split(stringSeparators, StringSplitOptions.None);
 
+            var count = 0;
             foreach (var item in lines)
             {
+                count++;
                 var linha = item.Trim();
+
                 if (!string.IsNullOrEmpty(linha))
                 {
                     if (VerificaSeTemCondicao(linha))
                     {
                         if (!VerificaSeECondicional(linha))
                         {
-                            return false;
+                            return "Erro: Estrutura condicional na linha " + count.ToString() + Environment.NewLine + "command: " + linha;
                         };
                     }
                     else if (VerificaSeTemRepeticao(linha))
                     {
                         if (!VerificaSeERepeticao(linha))
                         {
-                            return false;
+                            return "Erro: Estrutura de repetição na linha " + count.ToString() + Environment.NewLine + "command: " + linha;
                         }
                     }
                     else if (VerificaSeEVariavel(linha))
                     {
                         if (!VerificaSintaxeEPegaVariavel(linha))
                         {
-                            return false;
+                            return "Erro: Erro na declaração de var na linha " + count.ToString() + Environment.NewLine + "command: " + linha;
                         }
                     }
                     else
                     {
-                        return false;
+                        return "Erro:Comando não identificado na linha " + count.ToString() + Environment.NewLine + "command: " + linha;
                     }
                 }
             }
-            return valido;
+            return "Sucesso";
         }
 
         private bool VerificaSeTemCondicao(string linha)
         {
-            Regex rx = new Regex(@"^(" + string.Join("|", Db.EstruturaCondicao) + ")+(.*.)");
+            Regex rx = new Regex(@"^\s*(" + string.Join("|", Db.EstruturaCondicao) + ")+(.*.)");
             return rx.Matches(linha).Count() > 0;
         }
 
         private bool VerificaSintaxeEPegaVariavel(string linha)
         {
-            Regex rx = new Regex(@"^" + string.Join("|", Db.Tipos) + "\\s+(.*.)");
+            Regex rx = new Regex(@"^\s*(" + string.Join("|", Db.Tipos) + ")\\s+(.*.)");
             if (rx.Matches(linha).Count() == 0)
             {
                 return false;
@@ -88,7 +97,7 @@ namespace AnalisadorLexico.Controllers
 
         private bool VerificaSeECondicional(string linha)
         {
-            Regex rx = new Regex(@"^(" + string.Join("|", Db.EstruturaCondicao) + ")+\\(+(" + string.Join("|", Db.Variaveis) + ")+(" + string.Join("|", Db.CharsComparacao) + ")+(" + string.Join("|", Db.Variaveis) + ")+\\)+{$");
+            Regex rx = new Regex(@"^\s*(" + string.Join("|", Db.EstruturaCondicao) + ")\\s*\\(\\s*(" + string.Join("|", Db.Variaveis) + ")\\s*(" + string.Join("|", Db.CharsComparacao) + ")\\s*(" + string.Join("|", Db.Variaveis) + ")\\s*\\)\\s*$");
             return rx.Matches(linha).Count() > 0;
         }
 
@@ -100,13 +109,13 @@ namespace AnalisadorLexico.Controllers
 
         private bool VerificaSeERepeticao(string linha)
         {
-            Regex rx = new Regex(@"^(" + string.Join("|", Db.EstruturasRepeticao) + ")+\\(+(" + string.Join("|", Db.Variaveis) + ")+(" + string.Join("|", Db.CharsRepeticao) + ")+(" + string.Join("|", Db.Variaveis) + ")+\\)+{$");
+            Regex rx = new Regex(@"^\s*(" + string.Join("|", Db.EstruturasRepeticao) + ")\\s*\\(\\s*(" + string.Join("|", Db.Variaveis) + ")\\s*(" + string.Join("|", Db.CharsRepeticao) + ")\\s*(" + string.Join("|", Db.Variaveis) + ")\\s*\\)\\s*$");
             return rx.Matches(linha).Count() > 0;
         }
 
         private bool VerificaSeEVariavel(string linha)
         {
-            Regex rx = new Regex(@"^" + string.Join("|", Db.Tipos) + "");
+            Regex rx = new Regex(@"^\s*(" + string.Join("|", Db.Tipos) + ")");
             return rx.Matches(linha).Count() > 0;
         }
 
